427eaa4829122f48861da50b238bf78274d51a7f
mkassaian <matin.kassaian@gmail.com>
2018-05-18 18:36:20 -0700

    front end basically done

--- app/src/main/java/io/ocf/matink/croadslist/Main.java
+++ app/src/main/java/io/ocf/matink/croadslist/Main.java
@@ -1,7 +1,9 @@
 package io.ocf.matink.croadslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends AppCompatActivity {
 
@@ -11,6 +13,47 @@ public class Main extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //TOP PRIORITY: change plus in circle to regular plus ffs
         //get rid of top "croadslist" bar or change color u lazy fucks
+    }
 
+    public void open_listings(View view) {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void open_chat(View view) {
+        setContentView(R.layout.chat_screen);
+    }
+
+    public void open_profile(View view) {
+        setContentView(R.layout.myprofile_screen);
+        findViewById(R.id.gear_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_settings(v);
+            }
+        });
+        findViewById(R.id.my_listings_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_my_listings(v);
+            }
+        });
+        findViewById(R.id.transaction_history_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_transaction_history(v);
+            }
+        });
+    }
+
+    public void open_settings(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
+    }
+
+    public void open_my_listings(View view) {
+        startActivity(new Intent(this, MyListingsActivity.class));
+    }
+
+    public void open_transaction_history(View view) {
+        startActivity(new Intent(this, TransactionHistoryActivity.class));
     }
 }
